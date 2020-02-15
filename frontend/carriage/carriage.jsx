a2029caa3f617de4832bd294c14cedc5fd94629d
laszlovladi <laszlo.vladi@gmail.com>
2020-02-15 18:17:11 +0100

    Changed objects to strings in the array 'carriage' component

--- frontend/carriage/carriage.jsx
+++ frontend/carriage/carriage.jsx
@@ -3,16 +3,16 @@ import { Seat } from '../seat/seat.jsx';
 import './carriage.css';
 
 export let seats = [
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'occupied'},{occupancy: 'occupied'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'occupied'},{occupancy: 'occupied'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'occupied'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'occupied'}],
-  [{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'},{occupancy: 'free'}]
+  ['free', 'free', 'free', 'free'],
+  ['free', 'free', 'free', 'free'],
+  ['occupied', 'occupied', 'free', 'free'],
+  ['free', 'free', 'free', 'free'],
+  ['free', 'free', 'occupied', 'occupied'],
+  ['free', 'free', 'free', 'free'],
+  ['free', 'occupied', 'free', 'free'],
+  ['free', 'free', 'free', 'free'],
+  ['free', 'free', 'free', 'occupied'],
+  ['free', 'free', 'free', 'free']
   ];
 
 export default {
@@ -27,16 +27,15 @@ export class Carriage extends React.Component {
   };
   
   switch = (row, i, j) => {
-    console.log(seats);
-    if (row[j-1].occupancy === 'free') {
+    if (row[j-1] === 'free') {
       if (Carriage.previous !== 0){
-        seats[Math.floor((Carriage.previous-1)/4)][(Carriage.previous-1)%4] = {occupancy: "free"};
+        seats[Math.floor((Carriage.previous-1)/4)][(Carriage.previous-1)%4] = "free";
       }
-      seats[i][j-1] = {occupancy: "selected"};
+      seats[i][j-1] = "selected";
       this.setState({seats: seats});
       Carriage.previous = (i*4+j);
-    } else if (row[j-1].occupancy === 'selected') {
-      seats[i][j-1] = {occupancy: "free"};
+    } else if (row[j-1] === 'selected') {
+      seats[i][j-1] = "free";
       this.setState({seats: seats});
     }
   }
@@ -49,14 +48,14 @@ export class Carriage extends React.Component {
             return (
               <div className="carriage-row">
                 <Seat 
-                  occupancy={row[0].occupancy}
+                  occupancy={row[0]}
                   seatNumber={(i*4+1)}
                   handleClick={()=>{
                     this.switch(row, i, 1);
                   }}
                 />
                 <Seat
-                  occupancy={row[1].occupancy}
+                  occupancy={row[1]}
                   seatNumber={(i*4+2)}
                   handleClick={() => {
                     this.switch(row, i, 2);
@@ -64,14 +63,14 @@ export class Carriage extends React.Component {
                 />
                 <div className="aisle-spacer"></div>
                 <Seat 
-                  occupancy={row[2].occupancy}
+                  occupancy={row[2]}
                   seatNumber={(i*4+3)}
                   handleClick={() => {
                     this.switch(row, i, 3);
                   }}
                 />
                 <Seat 
-                  occupancy={row[3].occupancy}
+                  occupancy={row[3]}
                   seatNumber={(i*4+4)}
                   handleClick={() => {
                     this.switch(row, i, 4);
